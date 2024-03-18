5dcbe25886bf5214ab8b3f01fae7cde08cca6738
BelmerMB <elmermetrasbrum@gmail.com>
2024-03-18 01:01:52 -0300

    Show error

--- Sources/data_filter.py
+++ Sources/data_filter.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv('./Data/sales_data.csv', sep=',', encoding='Windows-1252')
-df.drop_duplicates(inplace=True)
+try:
+    df = pd.read_csv('./Data/sales_data.csv', sep=',', encoding='Windows-1252')
+except FileNotFoundError as fn:
+    print(f'File not found or doesnot exist, check the path and the name\nExiting...')
+    exit(0)
 
+
+
+df.drop_duplicates(inplace=True)
 df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
 
 
 #agrupamento por mês e ano de vendas
 df_mensal = df.set_index('ORDERDATE').groupby(pd.Grouper(freq='M')).sum().reset_index()
-print(df_mensal["SALES"])
-# irar colunas não essenciais a principio
+df_mensal['Ano'] = df_mensal['ORDERDATE'].dt.year
+df_mensal['mes'] = df_mensal['ORDERDATE'].dt.month_name(locale='Portuguese')
 # df.drop(columns=["ORDERNUMBER","MSRP","ORDERNUMBER","CUSTOMERNAME","PHONE","ADDRESSLINE1","ADDRESSLINE2", "POSTALCODE", "TERRITORY" ,"CONTACTLASTNAME","CONTACTFIRSTNAME"], inplace=True)
 # df.to_csv('sales_data_small.csv')
