d684a5e7eb58ca8bbdd297183bc38d1f8114b984
BelmerMB <elmermetrasbrum@gmail.com>
2024-03-18 13:00:23 -0300

    c

--- Sources/app.py
+++ Sources/app.py
@@ -4,8 +4,8 @@ from grapichs import receita_mensal_mundial
 #------Configuração inicial da página web------
 st.set_page_config(
     layout='wide',
-    page_title='Dashboard',
-    initial_sidebar_state='collapsed'
+    initial_sidebar_state='collapsed',
+    page_title='Dashboard'
 )
 
 tab1, tab2, tab3 = st.tabs(['World sells', 'Sales per country', 'Sales per model'])
@@ -18,3 +18,5 @@ with tab2:
     with columnOne:
         st.dataframe(df_mensal)
         st.plotly_chart(receita_mensal_mundial, use_container_width=True)
+    with columnTwo:
+        st.metric("Total de vendas:", df_mensal["SALES"].sum()/1000000)
