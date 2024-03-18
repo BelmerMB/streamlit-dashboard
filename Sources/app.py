2f8fb92a5740f85cccba482ff7573a27dfa2ebab
BelmerMB <elmermetrasbrum@gmail.com>
2024-03-18 01:01:15 -0300

    Abas e tabs

--- Sources/app.py
+++ Sources/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
-
+from data_filter import df, df_mensal
+from grapichs import receita_mensal_mundial
 #------Configuração inicial da página web------
 st.set_page_config(
     layout='wide',
@@ -8,4 +8,13 @@ st.set_page_config(
     initial_sidebar_state='collapsed'
 )
 
-tab1, tab2, tab3 = st.tabs(['World sells', 'Sales per country', 'Sales per model', 'Not shipped'])
+tab1, tab2, tab3 = st.tabs(['World sells', 'Sales per country', 'Sales per model'])
+
+with tab1:
+    st.dataframe(df)
+    st.write("hello")
+with tab2:
+    columnOne, columnTwo = st.columns(2)
+    with columnOne:
+        st.dataframe(df_mensal)
+        st.plotly_chart(receita_mensal_mundial, use_container_width=True)
